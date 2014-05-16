923ba258b2ffac8c751894f96c66c5ceccb5b8e4
Alexander Shirokov <alexander.shirokov@gmail.com>
2014-05-15 22:00:20 -0400

    Introduced debug.status

--- argpext/debug.py
+++ argpext/debug.py
@@ -2,7 +2,7 @@ import argpext
 from argpext import *
 
 #> Position 
-class FrameRef:
+class FrameRef(object):
     KEYS = keywords.KeyWords(['line','path','name','basename','smart_basename'])
 
     def __init__(self,up=0):
@@ -18,7 +18,8 @@ class FrameRef:
         elif key == 'path':
             return self.frame.f_code.co_filename
         elif key == 'name':
-            return self.frame.f_code.co_name
+            q = self.frame.f_code.co_name
+            return q
         elif key == 'basename':
             return os.path.basename(self['path'])
         elif key == 'smart_basename':
@@ -32,19 +33,46 @@ class FrameRef:
             raise KeyError()
 
 
-def chainref(fstr='%(name)s[%(smart_basename)s:%(line)s]',sep=' < ',up=0,limit=None):
+def chainref(fstr='{name}[{smart_basename}:{line}]',sep='|',up=0,limit=None):
     "Return chain reference."
     i = 0
     L = []
     while 1:
         try:
-            f = fstr % FrameRef(up=1+up+i)
+            f = fstr.format( **FrameRef(up=1+up+i) )
         except ValueError:
             break
         L += [f]
         i += 1
         if limit is not None and i == limit: break
     return sep.join(L)
+
+
+def status(**kwds):
+
+    up = kwds.pop('up',0)
+    s = kwds.pop('s',None)
+    e = kwds.pop('e',None)
+    n = kwds.pop('n',None)
+    if len(kwds): raise KeyError('unrecognized keys: %s' % ( ','.join(kwds.keys()) ) )
+
+    # Update the count
+    F = sys._getframe(1+up)
+    line = F.f_lineno
+    key = '__argpext_DebugPrint_%s' % line
+    count = F.f_globals.setdefault(key,-1)
+    count += 1
+    F.f_globals[key] = count
+
+    live = True
+    # Process the restrictions
+    if s is not None and count < s: live = False
+    if e is not None and count >= e: live = False
+
+    # Process the permissions
+    if n is not None and count == n: live = True
+
+    return dict(live=live,count=count)
 
 
 class DebugPrint(object):
@@ -67,30 +95,8 @@ class DebugPrint(object):
         # First thing off, check the highest priority argument.
         if not self.active: return
 
-        def active():
-            # Update the count
-            F = sys._getframe(2)
-            line = F.f_lineno
-            key = '__argpext_DebugPrint_%s' % line
-            count = F.f_globals.setdefault(key,-1)
-            count += 1
-            F.f_globals[key] = count
-
-            live = True
-            # Process the restrictions
-            s = kwds.pop('s')
-            e = kwds.pop('e')
-            if s is not None and count < s: live = False
-            if e is not None and count >= e: live = False
-
-            # Process the permissions
-            n = kwds.pop('n')
-            if n is not None and count == n: live = True
-
-            return count,live
-
-        count,live = active()
-        if not live: return
+        A = status(up=1,**dict([(k,kwds.pop(k,None)) for k in ['s','e','n']]))
+        if not A['live']: return
 
 
         # print arguments
@@ -103,7 +109,7 @@ class DebugPrint(object):
 
         frm = {}
         frm.update( FrameRef(up=1) )
-        frm['count'] = count
+        frm['count'] = A['count']
 
 
         string = sep.join([str(q) for q in args])+end
--- examples/debug.py
+++ examples/debug.py
@@ -4,8 +4,16 @@ import argpext
 
 pri = argpext.DebugPrint()
 
+def f():
+    for i in range(100000):
 
-for i in range(100000):
-    pri('here i=%d' % i,s=300,e=305,n=7)
+        pri('here i=%d' % i,s=300,e=305,n=7)
+
+        if argpext.debug.status(s=300,e=305,n=7)['live']:
+            print( argpext.chainref() )
+
+
+f()
+
 
 
