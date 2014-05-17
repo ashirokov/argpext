d69857b2b6c2f19e931081ccf3aa2a4400b03074
Alexander Shirokov <alexander.shirokov@gmail.com>
2014-05-16 22:31:08 -0400

    Fixed thedocs in setup.py

--- setup.py
+++ setup.py
@@ -12,29 +12,26 @@ setup(
     , scripts = ['argpext.py']
     , py_modules=['argpext']
     , license='OSI Approved'
-    , long_description="""Argpext is a module dedicated to improving the command line
-interface with Python module internals.  It allows one to
-quickly expose any selected Python functions to the command
-line within DOS or Linux-like shells. Help messages are
-automatically produced.
+    , long_description="""Argpext is a package that provides a collection of tools centred at
+research and infrastructure development.
 
-Argpext provides hierarchical extension to the
-"Sub-commands" utility of the standard argparse
-module. It allows one to group any Python functions into a
-hierarchical tree-like structure, e.g.  according to their
-logic. Every such function then corresponds to a certain
-sequence of sub-commands, and can be executed directly from
-the command line by simply passing the sequence as command
-line arguments to the top level script. The rest of the
-command line arguments to the script are used to set up the
-values of function arguments, at which level the standard
-argparse interface applies.
+It allows one to quickly expose any selected Python functions to command
+line interface for DOS or Linux-like shells. The user is responsible for organizing 
+the required Python functions into a hierarchical tree-like structure, according to their logic
+with tools provided by the package. Any such function then corresponds to a specific sequence of 
+command line arguments which, when executed 
+from the command line followed by the "--help" flag produces a detailed
+help message. In order to execute the function, the rest of the command 
+line arguments are filled based on the help message. 
 
-Argpext provides a special type to support command line
-arguments that take predetermined set of values. Information
-about available choices is automatically propagated into the usage help
-message.
+A detailed documentation is currently available only for version 1.1.
+Version 1.2.X will be documented within the next few weeks.
 
+Release v1.1 - multiple new features. Detailed documentation is provided.
+
+Release v1.2.0 - multiple new features
+
+Release v1.2.1 - bug fixes for v1.2.0
 """
     , classifiers = [
         'Development Status :: 4 - Beta'
