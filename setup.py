1c770e8b1c214dad9535550a5e5cc3d211a686f2
Alexander Shirokov <alexander.shirokov@gmail.com>
2014-07-13 21:36:47 -0400

    Source code Version 1.2.2 submitted

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     , author='Alexander Shirokov'
     , author_email='alexander.shirokov@gmail.com'
     , packages=['argpext']
-    , scripts = ['argpext.py']
+    #, scripts = ['argpext.py']
     , py_modules=['argpext']
     , license='OSI Approved'
     , long_description="""Argpext is a Python package that provides a collection of tools centred on
